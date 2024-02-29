5f50e1c70c8d8abf9164dc6f7a74b21937e7e108
farazmahmood1 <faraz.m4765@gmail.com>
2024-02-29 18:44:52 +0500

    implemented the simple if else condition in the chatbot

--- src/components/AIChatSection/AIChatSection.jsx
+++ src/components/AIChatSection/AIChatSection.jsx
@@ -1,186 +1,59 @@
 import React, { useEffect, useRef, useState } from "react";
 import './AiChatBotStyles.scss'
 import BotProfile from './Assets/chat-profile.png'
-import checkCircle from "./Assets/checkCircle.svg";
 import clipboard from "./Assets/clipboard.svg";
 
-import underConstruction from "../Images/undraw_under_construction_-46-pa.svg";
-// chatbot section
+// Chatbot section
 const AIChatSection = () => {
-
-  const initialMessage = {
-    role: "assistant",
-    content:
-      "",
-  };
-
-  const [socket, setSocket] = useState(null);
-  const [icon, setIcon] = useState("clipboard");
-  const [clickedMessageIndex, setClickedMessageIndex] = useState(null);
   const [message, setMessage] = useState("");
-  const [chat, setChat] = useState([initialMessage]);
-
-  const [loading, setLoading] = useState(false);
-  const [aiTyping, setAiTyping] = useState(false);
+  const [chat, setChat] = useState([]);
   const [disableInput, setDisableInput] = useState(false);
-  const lastMessegeRef = useRef(null);
+  const [aiTyping, setAiTyping] = useState(false);
+  const lastMessageRef = useRef(null);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setChat((prev) => [...prev]);
-    }, 50);
+  const staticQuestionsAndAnswers = [
+    {
+      question: "What services do you offer?",
+      answer: "I can help you with studying, brainstorming ideas, and generating business ideas.",
+    },
+    {
+      question: "How can I get started?",
+      answer: "You can start by asking me any questions you have!",
+    },
+    // Add more static questions and answers here as needed
+  ];
 
-    const socket = new WebSocket(
-      "wss://prod.eba-5bn4hjgy.eu-west-2.elasticbeanstalk.com/ws/socket-server/"
-    );
+  const sendMessage = () => {
+    if (message.trim() === "") return; // Don't send empty messages
 
-    socket.addEventListener("open", () => {
-      console.log("WebSocket connection opened.");
-    });
+    setChat(prevChat => [
+      ...prevChat,
+      { role: "user", content: message }
+    ]);
+    setMessage(""); // Clear input field
 
-    socket.addEventListener("error", (error) => {
-      console.log("WebSocket error", error);
-    });
-
-    socket.addEventListener("close", (event) => {
-      console.log("WebSocket connection closed", event);
-    });
-
-    setSocket(socket);
-
-    // return () => {
-    //     socket.close();
-    // }
-  }, [])
-
-  function handleCopyToClipboard(text, index) {
-    setClickedMessageIndex(index);
-    setIcon(checkCircle);
-
-    if (navigator.clipboard) {
-      navigator.clipboard.writeText(text).catch((error) => {
-        console.error("Unable to copy: " + error);
-      });
-    } else {
-      console.error("Clipboard API is not supported in this browser.");
-    }
-
-    setTimeout(() => {
-      setClickedMessageIndex(null);
-      setIcon(clipboard);
-    }, 1000);
-  }
-
-  const sendMessage = async () => {
+    // Simulate AI typing response
     setAiTyping(true);
-    const newData = {
-      message: [
-        ...chat,
-        {
-          role: "user",
-          content: message,
-        },
-      ],
-    };
-    setChat(newData.message);
-    setMessage("");
-    try {
-      if (socket) {
-        setLoading(true);
-        socket.send(JSON.stringify(newData));
+    setTimeout(() => {
+      const randomIndex = Math.floor(Math.random() * staticQuestionsAndAnswers.length);
+      const answer = staticQuestionsAndAnswers[randomIndex].answer;
 
-        socket.addEventListener("message", () => {
-          setAiTyping(false);
-        });
-      }
-    } catch (err) {
-      setChat((prev) => [
-        ...prev,
-        {
-          role: "assistant",
-          content: "Something went wrong on server.😟",
-        },
+      setChat(prevChat => [
+        ...prevChat,
+        { role: "assistant", content: answer }
       ]);
-    } finally {
-      setLoading(false);
-    }
+      setAiTyping(false);
+      scrollToBottom();
+    }, 1000);
+  };
+
+  const scrollToBottom = () => {
+    lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
-    try {
-      if (lastMessegeRef.current) {
-        lastMessegeRef.current?.scrollIntoView({ behavior: "smooth" });
-      }
-    } catch (error) {
-      console.error(error)
-    }
+    scrollToBottom();
   }, [chat]);
-
-  useEffect(() => {
-    const keyDownHandler = (event) => {
-      if (event.key === "Enter") {
-        event.preventDefault();
-        if (message && !loading) {
-          sendMessage();
-        }
-      }
-    };
-    document.addEventListener("keydown", keyDownHandler);
-    return () => {
-      document.removeEventListener("keydown", keyDownHandler);
-    };
-  }, [message]);
-
-  useEffect(() => {
-    if (socket) {
-      socket.onopen = (event) => {
-        console.log("Connection established");
-      };
-
-      socket.onmessage = (e) => {
-        setDisableInput(true);
-        const message_ = JSON.parse(e.data);
-        console.log(message_);
-
-        setTimeout(() => {
-          // setDis(false);
-
-          setDisableInput(false);
-          if (message_.message) {
-            // setDis(false);
-          }
-
-          if (chat.length === 0 || chat[chat.length - 1].role === 'user') {
-            // If there's no assistant message or the last message was from the user,
-            // add a new response box
-            setChat((prev) => [
-              ...prev,
-              {
-                role: 'assistant',
-                content: message_.message,
-              },
-            ]);
-          } else {
-            setChat((prev) => [
-              ...prev.slice(0, prev.length - 1),
-              {
-                role: 'assistant',
-                content: prev[prev.length - 1].content + message_.message,
-              },
-            ]);
-          }
-        });
-      };
-
-      socket.onerror = function (error) {
-        console.log("error", error);
-      };
-
-      socket.onclose = function (event) {
-        console.log(event);
-      };
-    }
-  }, [socket, chat]);
 
   return (
     <div>
@@ -189,156 +62,50 @@ const AIChatSection = () => {
           <div className='chat-bot-wrapper'>
             <div className="messageWrapper">
               <div className="chatMessage">
-                <div className="messageWrap">
-                  <img src={BotProfile} alt="" />
-                  <div className="message">
-                    <div className="messageHeader">
-                      <span
-                        style={{
-                          display: "block",
-                          textAlign: "right",
-                          cursor: "pointer",
-                        }}
-                        onClick={() =>
-                          handleCopyToClipboard(
-                            "Hello! How can I assist you today?",
-                            "header"
-                          )
-                        }
-                      >
-                        <img
-                          src={clickedMessageIndex === "header" ? icon : clipboard}
-                          alt="icon"
-                          style={{ width: "17px" }}
-                        />
-                      </span>
-                    </div>
-                    Hello! Let's get started! <br /> I can help you with:
-                    <br />
-                    <p style={{marginTop:"15px"}}>
-                    - Help you study<br />
-                    - Brainstorming Ideas<br />
-                    - Generating business ideas<br />
-                    </p>
-                  </div>
-                </div>
-
-
-                {chat
-                  .filter((item, index) => index !== 0) // Exclude the initial message
-                  .map(
-                    (item, i) =>
-                      item.role !== "system" && (
-                        <div
-                          key={i}
-                          ref={
-                            chat.length && chat.length - 2 <= i
-                              ? lastMessegeRef
-                              : null
-                          }
-                          className={
-                            item.role === "assistant"
-                              ? "messageWrap"
-                              : "messageWrapRight"
-                          }
-                        >
-                          {item.role === "assistant" ? (
-                            <>
-                              <img src={BotProfile} className="" alt="assistant profile" />
-                              <div className="message">
-                                <span
-                                  style={{
-                                    display: "block",
-                                    textAlign: "right",
-                                    cursor: "pointer",
-                                  }}
-                                  onClick={() =>
-                                    handleCopyToClipboard(item.content, i)
-                                  }
-                                >
-                                  <img
-                                    src={i === clickedMessageIndex ? icon : clipboard}
-                                    alt="icon"
-                                    style={{ width: "17px" }}
-                                  />
-                                </span>
-
-                                <div
-                                  id="aiMessage"
-                                  dangerouslySetInnerHTML={{ __html: item.content }}
-                                />
-
-                                <span
-                                  style={{
-                                    display: "block",
-                                    textAlign: "right",
-                                    fontSize: "12px",
-                                    color: "GrayText",
-                                  }}
-                                >
-                                  {item.timestamp}
-                                </span>
-                              </div>
-                            </>
-                          ) : (
-                            <>
-                              <div className="message">
-                                {item.content}
-                                <span
-                                  style={{
-                                    display: "block",
-                                    textAlign: "right",
-                                    fontSize: "12px",
-                                    color: "GrayText",
-                                  }}
-                                >
-                                  {item.timestamp}
-                                </span>
-                              </div>
-                              <div
-                                style={{
-                                  height: "30px",
-                                  width: "30px",
-                                  backgroundColor: "#232323",
-                                  borderRadius: "25px",
-                                }}
-                              >
-                                <i className="fa-regular fa-user text-white ms-2 mt-1" />
-                              </div>
-                            </>
-                          )}
+                {chat.map((item, index) => (
+                  <div
+                    key={index}
+                    className={item.role === "assistant" ? "messageWrap" : "messageWrapRight"}
+                  >
+                    {item.role === "assistant" && (
+                      <>
+                        <img src={BotProfile} className="" alt="assistant profile" />
+                        <div className="message">
+                          {item.content}
                         </div>
-                      )
-                  )}
+                      </>
+                    )}
+                    {item.role === "user" && (
+                      <div className="message">
+                        {item.content}
+                      </div>
+                    )}
+                  </div>
+                ))}
+                {/* Empty div for scrolling to the bottom */}
+                <div ref={lastMessageRef} />
               </div>
             </div>
-
             <div className='bottom-content'>
               <div className='container'>
                 <hr />
                 <div className='d-flex'>
-                  <input className="form-control pt-3 pb-3 me-1" type="search" aria-label="Search"
-                    disabled={disableInput || aiTyping}
-                    name="message"
+                  <input
+                    className="form-control pt-3 pb-3 me-1"
+                    type="text"
                     value={message}
-                    variant="outlined"
+                    onChange={e => setMessage(e.target.value)}
                     placeholder="Message"
-                    onChange={(e) => setMessage(e.target.value)}
+                    disabled={disableInput || aiTyping}
                     style={{ borderRadius: "10px" }}
                   />
-
-                  {
-                    message !== "" ? (
-                      <button className='btn btn-outline-dark ms-1 rounded-3' variant="contained"
-                        onClick={sendMessage}
-                      > <i className="fa-solid fa-paper-plane" />
-                      </button>
-                    ) : (
-                      <button className='btn btn-outline-dark ms-1 rounded-3' variant="contained" > <i className="fa-solid fa-paper-plane" />
-                      </button>
-                    )
-                  }
-
+                  <button
+                    className='btn btn-outline-dark ms-1 rounded-3'
+                    onClick={sendMessage}
+                    disabled={disableInput || aiTyping}
+                  >
+                    <i className="fa-solid fa-paper-plane" />
+                  </button>
                 </div>
               </div>
             </div>
